4424b24d6640854a17282c5e48a21ab9a9ee7dd4
clover <ThreeLeafClover9@gmail.com>
2025-01-24 13:42:31 +0900

    user service api

---
+++ src/main/java/com/coworkerweb/myRestfulService/controller/UserController.java
@@ -0,0 +1,58 @@
+package com.coworkerweb.myRestfulService.controller;
+
+import com.coworkerweb.myRestfulService.bean.User;
+import com.coworkerweb.myRestfulService.dao.UserDaoService;
+import com.coworkerweb.myRestfulService.exception.UserNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class UserController {
+    private UserDaoService userDaoService;
+
+    public UserController(UserDaoService userDaoService) {
+        this.userDaoService = userDaoService;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return userDaoService.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        User user = userDaoService.findOne(id);
+
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        return user;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = userDaoService.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity deleteUser(@PathVariable int id) {
+        User deletedUser = userDaoService.deleteById(id);
+
+        if (deletedUser == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        return ResponseEntity.noContent().build();
+    }
+}
