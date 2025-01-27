96c7cf5497fe49c41ea29172670bf0ad5d909be6
clover <threeleafclover9@gmail.com>
2025-01-27 16:27:33 +0900

    filtering

---
+++ src/main/java/com/coworkerweb/myRestfulService/controller/AdminUserController.java
@@ -0,0 +1,66 @@
+package com.coworkerweb.myRestfulService.controller;
+
+import com.coworkerweb.myRestfulService.bean.AdminUser;
+import com.coworkerweb.myRestfulService.bean.User;
+import com.coworkerweb.myRestfulService.dao.UserDaoService;
+import com.coworkerweb.myRestfulService.exception.UserNotFoundException;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminUserController {
+    private UserDaoService userDaoService;
+
+    public AdminUserController(UserDaoService userDaoService) {
+        this.userDaoService = userDaoService;
+    }
+
+    @GetMapping("/users")
+    public MappingJacksonValue retrieveAllUsersForAdmin() {
+        List<User> users = userDaoService.findAll();
+
+        ArrayList<AdminUser> adminUsers = new ArrayList<>();
+        AdminUser adminUser = null;
+        for (User user : users) {
+            adminUser = new AdminUser();
+            BeanUtils.copyProperties(user, adminUser);
+            adminUsers.add(adminUser);
+        }
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("id", "name", "joinDate", "ssn");
+        SimpleFilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(adminUsers);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+
+    @GetMapping("/users/{id}")
+    public MappingJacksonValue retrieveUserForAdmin(@PathVariable int id) {
+        User user = userDaoService.findOne(id);
+
+        AdminUser adminUser = new AdminUser();
+
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        } else {
+            BeanUtils.copyProperties(user, adminUser);
+        }
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("id", "name", "joinDate", "ssn");
+        SimpleFilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(adminUser);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+}
