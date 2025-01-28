109438fc1494ed5d15f0d5e69aa037eae0db5524
clover <threeleafclover9@gmail.com>
2025-01-28 19:37:51 +0900

    jpa config, initialization

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into users(id, join_date, name, password, ssn) values(1, now(), 'Kenneth', 'test1', '111111-1111111');
+insert into users(id, join_date, name, password, ssn) values(2, now(), 'Alice', 'test2', '222222-1111111');
+insert into users(id, join_date, name, password, ssn) values(3, now(), 'Elena', 'test3', '333333-1111111');
